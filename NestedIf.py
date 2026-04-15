0179f20478761313d915dd1c3a3dd6c84d539e22
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-04-15 15:32:42 +0530

    commit add

---
+++ NestedIf.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+
+# num = 10
+if num > 0:
+    print("The number is positive.")
+    if num % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+else:
+    print("The number is negative.")    
---
+++ nestedLoop.py
@@ -0,0 +1,6 @@
+num = 10
+for i in range(1, num + 1):
+    for j in range(1, num + 1):
+        print(i, "*", j, "=", i * j)
+    print()  # Print a new line after each row of the multiplication table
+    
