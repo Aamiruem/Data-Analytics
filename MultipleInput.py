32d8c173ad74c3bba110c44f649610d7b36d95d5
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-04-15 11:55:23 +0530

    commit add

---
+++ MultipleInput.py
@@ -0,0 +1,5 @@
+# a, b = input(int("Enter the value of a and b"))
+a = 10
+b = 20
+a, b = map(int, input().split())
+print(a + b)
---
+++ dataConversion.py
@@ -0,0 +1,4 @@
+x = "10"
+
+print(int(x), "convert string to GetInteger(field)")  # string → int
+print(float(x), "convert string to GetFloat(field)")  # string → float
